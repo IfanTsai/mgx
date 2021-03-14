4fee74a44a81f29f0232967c223f514e02fc4c9a
IfanTsai <1558499691@qq.com>
2021-03-14 20:09:31 +0800

    add handle of POST method request

--- http/mgx_http_socket.cpp
+++ http/mgx_http_socket.cpp
@@ -9,7 +9,9 @@ extern Mgx_th_pool g_mgx_th_pool;
 
 Mgx_http_socket::Mgx_http_socket()
 {
-
+    mime_types_init();
+    Mgx_conf *mgx_conf = Mgx_conf::get_instance();
+    m_index_path = mgx_conf->get_string(CONFIG_IndexPath, DEFAULT_INDEX_PATH);
 }
 
 Mgx_http_socket::~Mgx_http_socket()
@@ -20,97 +22,109 @@ Mgx_http_socket::~Mgx_http_socket()
 bool Mgx_http_socket::init()
 {
     // ...
-    Mgx_conf *mgx_conf = Mgx_conf::get_instance();
-    index_path = mgx_conf->get_string(CONFIG_IndexPath, DEFAULT_INDEX_PATH);
 
     return Mgx_socket::init();
 }
 
-void Mgx_http_socket::_wait_request_handler(pmgx_conn_t c)
+void Mgx_http_socket::_read_request_handler(pmgx_conn_t c)
 {
     mgx_http_request_t http_req;
-    ssize_t recv_size = http_read_request_line(c, &http_req);
-    if (recv_size <= 0)
+
+    if (http_read_request_line(c, &http_req) <= 0)
         return;
 
-    http_parse_request_line(&http_req);
-
-    recv_size = http_read_request_head(c, &http_req);
-    if (recv_size <= 0)
+    if (!http_parse_request_line(&http_req))
         return;
 
-    http_parse_request_head(&http_req);
+    if (http_read_request_head(c, &http_req) <= 0)
+        return;
 
-    http_read_request_body(c, &http_req);
+    if (!http_parse_request_head(&http_req))
+        return;
+
+    if (http_read_request_body(c, &http_req) <= 0)
+        return;
+
+    uint tmp_buf_size = m_msg_hdr_size + 1;
+    if (http_req.request_body)
+        tmp_buf_size += strlen(http_req.request_body);
+
+    char *tmp_buf = new char[tmp_buf_size]();
+    c->precv_mem_addr = tmp_buf;
+
+    pmgx_msg_hdr_t pmsg_hdr = (pmgx_msg_hdr_t)tmp_buf;
+    pmsg_hdr->pconn = c;
+    pmsg_hdr->cur_seq = c->m_cur_seq;
+    pmsg_hdr->uri = new char[strlen(http_req.uri) + 1]();
+    strcpy(pmsg_hdr->uri, http_req.uri);
 
     if (http_req.method == METHOD_GET) {
-        char *tmp_buf = new char[m_msg_hdr_size + strlen(http_req.uri) + 1]();
-        c->precv_mem_addr = tmp_buf;
-
-        pmgx_msg_hdr_t pmsg_hdr = (pmgx_msg_hdr_t)tmp_buf;
-        pmsg_hdr->pconn = c;
-        pmsg_hdr->cur_seq = c->m_cur_seq;
         pmsg_hdr->http_method = METHOD_GET;
+    } else if (http_req.method == METHOD_POST) {
+        pmsg_hdr->http_method = METHOD_POST;
         tmp_buf += m_msg_hdr_size;
-        strcpy(tmp_buf, http_req.uri);
+        strcpy(tmp_buf, http_req.request_body);
+    } else {
 
-        g_mgx_th_pool.insert_msg_to_queue_and_signal(c->precv_mem_addr);
     }
+
+    g_mgx_th_pool.insert_msg_to_queue_and_signal(c->precv_mem_addr);
 }
 
 void Mgx_http_socket::th_msg_process_func(char *buf)
 {
-    mgx_log(MGX_LOG_DEBUG, "receive get: %s", buf + m_msg_hdr_size);
     pmgx_msg_hdr_t pmsg_hdr = (pmgx_msg_hdr_t)buf;
-    buf += m_msg_hdr_size;
+    const char *request_body = buf + m_msg_hdr_size;
+    char *uri = pmsg_hdr->uri;
 
     mgx_http_response_t http_res;
     http_res.headers["Server"] = "Mgx http server";
+    http_res.headers["Connection"] = "keep-alive";
     if (pmsg_hdr->http_method == METHOD_GET) {
-        std::string static_file_path = index_path + buf;
-        if (!strcmp(buf, "/"))
+        std::string static_file_path = m_index_path + uri;
+        if (!strcmp(uri, "/"))
             static_file_path += "index.html";
 
-        mgx_log(MGX_LOG_DEBUG, "static_file_path: %s", static_file_path.c_str());
         if (!access(static_file_path.c_str(), F_OK)) {
             std::string res_body;
             read_file_all(static_file_path.c_str(), res_body);
 
             http_res.status_line = "HTTP/1.1 " HTTP_STATUS_LINE(200);
-            if (strstr(buf, ".css"))
-                http_res.headers["Content-Type"] = "text/css";
-            else if (strstr(buf, ".js"))
-                http_res.headers["Content-Type"] = "application/javascript";
-            else if (strstr(buf, ".jpeg") || strstr(buf, ".jpg"))
-                http_res.headers["Content-Type"] = "image/jpeg";
-            else
-                http_res.headers["Content-Type"] = "text/html; charset=UTF-8";
-
-            http_res.headers["Connection"] = "keep-alive";
+            http_res.headers["Content-Type"] = get_mime_type(get_extension(uri));
             http_res.headers["Content-Length"] = std::to_string(res_body.size()).c_str();
             http_res.response_body = new char[res_body.size() + 1]();
             memcpy(http_res.response_body, res_body.c_str(), res_body.size());
         } else {
             std::string res_body;
-            if (strcmp(buf, "/favicon.ico"))
+            if (strcmp(uri, "/favicon.ico"))
                 read_file_all("./http/html/404.html", res_body);
 
             http_res.status_line = "HTTP/1.1 " HTTP_STATUS_LINE(404);
             http_res.headers["Content-Type"] = "text/html; charset=UTF-8";
-            http_res.headers["Connection"] = "keep-alive";
             http_res.headers["Content-Length"] = std::to_string(res_body.size()).c_str();
             http_res.response_body = new char[res_body.size() + 1]();
             memcpy(http_res.response_body, res_body.c_str(), res_body.size());
         }
+    } else if (pmsg_hdr->http_method == METHOD_POST) {
+        mgx_log(MGX_LOG_DEBUG, "request_body: %s", request_body);
+        std::string res_body = request_body;  /* echo for test */
+
+        http_res.status_line = "HTTP/1.1 " HTTP_STATUS_LINE(200);
+        http_res.headers["Content-Type"] = get_mime_type(".json");
+        http_res.headers["Content-Length"] = std::to_string(res_body.size()).c_str();
+        http_res.response_body = new char[res_body.size() + 1]();
+        memcpy(http_res.response_body, res_body.c_str(), res_body.size());
     }
 
     http_write_response(&http_res, pmsg_hdr);
+
+    delete[] uri;
 }
 
-int Mgx_http_socket::http_read_request_line(pmgx_conn_t c, pmgx_http_request_t phttp_req)
+ssize_t Mgx_http_socket::http_read_request_line(pmgx_conn_t c, pmgx_http_request_t phttp_req)
 {
     ssize_t recv_size;
-    int len = 0;
+    ssize_t len = 0;
     char ch;
     for (;;) {
         recv_size = recv_process(c, &ch, 1);
@@ -136,27 +150,36 @@ int Mgx_http_socket::http_read_request_line(pmgx_conn_t c, pmgx_http_request_t p
     return len;
 }
 
-int Mgx_http_socket::http_parse_request_line(pmgx_http_request_t phttp_req)
+bool Mgx_http_socket::http_parse_request_line(pmgx_http_request_t phttp_req)
 {
     char *request_line = phttp_req->request_line;
-    if (!strncmp(request_line, "GET", strlen("GET"))) {
-        phttp_req->method = METHOD_GET;
-        char *p = strstr(request_line, "HTTP/");
-        phttp_req->http_version = p;
 
-        int url_len = p - request_line - strlen("GET") - 2;
-        char *uri = new char[url_len + 1]();
-        strncpy(uri, request_line + strlen("GET") + 1, url_len);
-        phttp_req->uri = uri;
+    char method_str[16] = { 0 };
+    if (!strncmp(request_line, "GET", strlen("GET"))) {
+        strcpy(method_str, "GET");
+        phttp_req->method = METHOD_GET;
+    } else if (!strncmp(request_line, "POST", strlen("POST"))) {
+        strcpy(method_str, "POST");
+        phttp_req->method = METHOD_POST;
+    } else {
+        return false;
     }
 
-    return 0;
+    char *p = strstr(request_line, "HTTP/");
+    phttp_req->http_version = p;
+
+    int url_len = p - request_line - strlen(method_str) - 2;
+    char *uri = new char[url_len + 1]();
+    strncpy(uri, request_line + strlen(method_str) + 1, url_len);
+    phttp_req->uri = uri;
+
+    return true;
 }
 
-int Mgx_http_socket::http_read_request_head(pmgx_conn_t c, pmgx_http_request_t phttp_req)
+ssize_t Mgx_http_socket::http_read_request_head(pmgx_conn_t c, pmgx_http_request_t phttp_req)
 {
     ssize_t recv_size;
-    int len = 0;
+    ssize_t len = 0;
     char ch;
     for (;;) {
         recv_size = recv_process(c, &ch, 1);
@@ -188,27 +211,42 @@ int Mgx_http_socket::http_read_request_head(pmgx_conn_t c, pmgx_http_request_t p
     return len;
 }
 
-int Mgx_http_socket::http_parse_request_head(pmgx_http_request_t phttp_req)
+bool Mgx_http_socket::http_parse_request_head(pmgx_http_request_t phttp_req)
 {
     const char *delim = CRLF;
     for (char *p = strtok(phttp_req->request_head, delim); p; p = strtok(nullptr, delim)) {
         std::string line(p);
         /* split by ':', get key and value */
         int index = line.find_first_of(':');
-        std::string key = line.substr(0, index - 1);
-        std::string val = line.substr(index + 1, line.size() - 1);
+        std::string key = line.substr(0, index);
+        std::string val = line.substr(index + 1, line.size() - key.size());
 
         /* put key and value in headers map */
         phttp_req->headers[strim(key)] = std::move(strim(val));
     }
 
-    return 0;
+    return true;
 }
 
-int Mgx_http_socket::http_read_request_body(pmgx_conn_t c, pmgx_http_request_t phttp_req)
+ssize_t Mgx_http_socket::http_read_request_body(pmgx_conn_t c, pmgx_http_request_t phttp_req)
 {
+    if (!phttp_req->headers.count("Content-Length"))
+        return 1;
 
-    return 0;
+    uint content_len = stoi(phttp_req->headers["Content-Length"]);
+    mgx_log(MGX_LOG_DEBUG, "Content-Length: %d", content_len);
+
+    phttp_req->request_body = new char[content_len + 1]();
+
+    ssize_t recv_size, off = 0;
+    while (off < content_len) {
+        recv_size = recv_process(c, phttp_req->request_body + off, content_len - off);
+        if (recv_size <= 0)
+            return recv_size;
+        off += recv_size;
+    }
+
+    return content_len;
 }
 
 int Mgx_http_socket::http_write_response(pmgx_http_response_t phttp_res, pmgx_msg_hdr_t pmsg_hdr)
@@ -250,4 +288,38 @@ int Mgx_http_socket::read_file_all(const char *file_path, std::string &buf)
     buf = ss.str();
 
     return 0;
+}
+
+inline void Mgx_http_socket::mime_types_init()
+{
+    m_mime_types = {
+        { ".html", "text/html" },
+        { ".htm",  "text/html" },
+        { ".xml",  "text/xml" },
+        { ".txt",  "text/plain" },
+        { ".css",  "text/css" },
+        { ".js",   "application/javascript" },
+        { ".json", "application/json" },
+        { ".zip",  "application/zip" },
+        { ".jpeg", "image/jpeg" },
+        { ".jpg",  "image/jpeg" },
+        { ".png",  "image/png" },
+        { ".gif",  "image/gif" },
+        { ".ico",  "image/x-icon" },
+        { ".bmp",  "image/x-ms-bmp" },
+    };
+}
+
+inline const char *Mgx_http_socket::get_mime_type(const char *extension)
+{
+    return m_mime_types[extension];
+}
+
+const char *Mgx_http_socket::get_extension(const char *path)
+{
+    const char *p = path + strlen(path);
+    while (*p != '.' && p != path)
+        p--;
+
+    return p != path ? p : ".html";
 }
